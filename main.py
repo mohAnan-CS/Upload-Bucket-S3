b8cf185cbc8af015fcef40ad37d62539fca467f5
Mohammad Anan <smoker_shuger@hotmail.com>
2022-12-15 23:13:07 +0200

    Add file upload to s3 bucket

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from constants import *
+import boto3
+import os
+
+client = boto3.client('s3',
+                      aws_access_key_id="AKIA3VWWH75JIKV4SDRZ",
+                      aws_secret_access_key="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"
+                      )
+
+root_path = os.getcwd()
+print(f"root_path : {root_path}")
+file_name = "receipt.txt"
+
+file_path = os.path.join(root_path, file_name)
+print(f"file_path : {file_path}")
+
+bucket_name = "mohammadbucket"
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+client.upload_file(file_path, bucket_name, file_name)
